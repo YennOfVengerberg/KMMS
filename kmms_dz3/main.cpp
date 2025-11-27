1d598d83ad010a273f14595b2ae6d2c46cbcf225
Alexey <500000vasa100@mail.ru>
2025-11-27 17:49:27 +0300

    init

---
+++ kmms_dz3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int input;
+
+    do {
+        std::cout << "Супер крутое меню!3131 " << std::endl;
+        std::cin >> input;
+        switch (input) {
+            case 0: {
+                exit(0);
+            }
+            case 1: {
+
+            }
+        }
+        std::cout << std::endl;
+    } while(true);
+
+}
