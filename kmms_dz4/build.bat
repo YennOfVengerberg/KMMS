ec6cad59de23e22e33b35c09144d6b2c91458e61
Alexey <500000vasa100@mail.ru>
2025-12-16 20:57:18 +0300

    added simple test for merge sort for correctness

--- kmms_dz4/build.bat
+++ kmms_dz4/build.bat
@@ -13,8 +13,16 @@ cd %BUILD_FOLDER%
 cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
 cmake --build .
 
+set arr[0].file=sort.bat
+
+set arr[0].folder=sort
+
 copy ..\%SOURCE_FOLDER%\test\run.bat .\test
 copy ..\%SOURCE_FOLDER%\menu\run_menu.bat .\menu
-copy ..\%SOURCE_FOLDER%\sort\sort.bat .\sort
+copy ..\%SOURCE_FOLDER%\sort\sort.bat .\src
+
+copy ..\%SOURCE_FOLDER%\%arr[0].folder%\%arr[0].file% .\%arr[0].folder%
+
+copy ..\run_tests.bat .
 
 pause
