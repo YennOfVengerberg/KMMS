d9d9c4275e73db0ddf5c43f53a128d9b0039f7b8
Alexey <500000vasa100@mail.ru>
2026-02-23 16:57:36 +0300

    initial cmake setup for dz

---
+++ kmms_longnums_dz/build.bat
@@ -0,0 +1,16 @@
+set BUILD_TYPE=Ninja
+set BUILD_SUFFIX=ninja
+
+chcp 65001
+
+set BUILD_FOLDER=build_%BUILD_SUFFIX%
+set SOURCE_FOLDER=.
+
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+
+cd %BUILD_FOLDER%
+
+cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
+cmake --build .
+
+pause
---
+++ kmms_longnums_dz/tests/tests.cpp
@@ -0,0 +1,23 @@
+//#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "sort.hpp"
+#include "io.hpp"
+
+TEST(ArraysEquality, Equal) {
+    const int size = 10;
+    int given[size] = {12, 53, 0, 7, 11, 12, 11, 3, 4, 5};
+    yenni::modified_merge_sort(given, 0, size - 1, 5, 10);
+    int expected[size] = {0, 3, 4, 7 /*5*/, 7, 11, 11, 12, 12, 53}; // специально неправильно
+
+    for(int i = 0; i < size; i++ ){
+        ASSERT_EQ(given[i], expected[i])
+            << "неравные значения в индексе " << i;
+    }
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
