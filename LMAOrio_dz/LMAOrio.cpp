f2de58b26c4493d57e1559a9405ec3d723164837
Alexey <500000vasa100@mail.ru>
2026-04-28 22:37:16 +0300

    movement, initial map, collisions

--- LMAOrio_dz/LMAOrio.cpp
+++ LMAOrio_dz/LMAOrio.cpp
@@ -11,11 +11,13 @@ typedef struct SObject {
     float x, y;
     float height, width;
     float vert_speed;
+    BOOL in_air;
 } TObject;
 
 char map[map_height][map_width+1];
 TObject memerio;
-TObject brick[1];
+TObject *brick = NULL;
+int brick_length;
 
 void clear_map() {
     for(int i = 0; i < map_width; i++) 
@@ -45,12 +47,31 @@ void init_object(TObject *obj, float x_pos, float y_pos, float o_width, float o_
 BOOL is_collision(TObject obj1, TObject obj2);
 
 void vert_move_object(TObject *obj) {
+    (*obj).in_air = TRUE;
     (*obj).vert_speed += 0.05;
     set_object_pos(obj, (*obj).x, (*obj).y + (*obj).vert_speed);
-    if(is_collision(*obj, brick[0] ) ) {
-        (*obj).y -= (*obj).vert_speed;
-        (*obj).vert_speed = 0;
+
+    for(int i = 0; i < brick_length; i++ ) {    
+        if(is_collision(*obj, brick[i] ) ) {
+            (*obj).y -= (*obj).vert_speed;
+            (*obj).vert_speed = 0;
+            (*obj).in_air = FALSE;
+            break;
+        }
     }
+}
+
+void create_level() {
+    init_object(&memerio, 39, 10, 3, 3);
+
+    brick_length = 5;
+    brick = (TObject*)realloc(brick, sizeof(*brick) * brick_length);
+    init_object(brick+0, 20, 20, 40, 5);
+    init_object(brick+1, 60, 15, 10, 10);
+    init_object(brick+2, 80, 20, 20, 5);
+    init_object(brick+3, 120, 15, 10, 10);
+    init_object(brick+4, 155, 20, 40, 5);
+
 }
 
 BOOL is_pos_in_map(int x, int y) {
@@ -76,19 +97,42 @@ void set_cursor(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void horizontal_move_map(float dx) {
+    memerio.x -= dx;
+    for(int i = 0; i < brick_length; i++) {
+        if(is_collision(memerio, brick[i])) {
+            memerio.x += dx;
+            return;
+        }
+    }
+    memerio.x += dx;
+
+    for(int i = 0; i < brick_length; i++) 
+        brick[i].x += dx;
+}
+
 BOOL is_collision(TObject obj1, TObject obj2) {
     return (obj1.x + obj1.width > obj2.x && obj1.x < obj2.x + obj2.width && 
             obj1.y + obj1.width > obj2.y && obj1.y < obj2.y + obj2.height); 
 }
 
 int main() {
-    init_object(&memerio, 39, 10, 3, 3);
-    init_object(brick, 20, 20, 40, 5);
+    
+    create_level();
 
     do  {
         clear_map();
+
+        if(memerio.in_air == FALSE && GetKeyState(VK_UP) < 0) memerio.vert_speed = -1.0;
+        if(GetKeyState(VK_LEFT) < 0) horizontal_move_map(1);
+        if(GetKeyState(VK_RIGHT) < 0) horizontal_move_map(-1);
+
+        if(memerio.y > map_height) create_level();
+
         vert_move_object(&memerio);
-        put_object_on_map(brick[0]);
+        for(int i = 0; i < brick_length; i++) {
+            put_object_on_map(brick[i]);
+        }
         put_object_on_map(memerio);
 
         set_cursor(0, 0);
